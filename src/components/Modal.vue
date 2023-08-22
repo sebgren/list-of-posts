58f76d7f0a2631457538a497b30c8901bd3f893b
unknown <seho3066@gmail.com>
2023-08-22 22:35:29 +0200

    added additional items to modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,32 +1,69 @@
 <script setup>
-import { ref } from 'vue'
-
-const post = ref({})
-const modalRef = ref(null)
-const parser = new DOMParser()
+import { ref } from "vue";
+import {
+  LinkIcon,
+  UserCircleIcon,
+  ChatBubbleBottomCenterTextIcon,
+  StarIcon,
+  PencilSquareIcon,
+} from "@heroicons/vue/24/outline";
+const post = ref({});
+const modalRef = ref(null);
+const parser = new DOMParser();
 
 defineExpose({
-  openModal
-})
+  openModal,
+});
 
 function openModal(newPost) {
-  post.value = newPost
-  modalRef.value.showModal()
+  post.value = newPost;
+  modalRef.value.showModal();
 }
-
 </script>
 
 <template>
-<!-- Open the modal using ID.showModal() method -->
-<dialog v-show="post" id="post-modal" ref="modalRef" class="modal">
-  <form method="dialog" class="modal-box max-w-4xl">
-    <h3 class="font-bold text-lg">{{ post.title || ''}}</h3>
-    <figure v-if="post.thumbnail" class="flex justify-center"><img :src="post.thumbnail" /></figure>
-    <p class="py-4" v-html="parser.parseFromString(post.selftext_html || '', 'text/html').body.textContent"></p>
-  </form>
-  <form method="dialog" class="modal-backdrop">
-    <button>close</button>
-  </form>
-</dialog> 
-</template>
+  <!-- Open the modal using ID.showModal() method -->
+  <dialog v-show="post" id="post-modal" ref="modalRef" class="modal">
+    <form method="dialog" class="modal-box max-w-4xl">
+      <h3 class="font-bold text-lg">{{ post.title || "" }}</h3>
+      <p class="flex pt-2">
+        <LinkIcon class="w-6 h-6" />
+        <a
+          class="inline-block w-[274px] link link-hover link-primary text-ellipsis overflow-hidden whitespace-nowrap"
+          target="_blank"
+          rel="noreferrer noopener"
+          :href="post.url"
+          >{{ post.permalink }}</a
+        >
+      </p>
+      <figure class="flex justify-center">
+        <img :src="post.thumbnail" />
+      </figure>
+      <p
+        class="py-4"
+        v-html="
+          parser.parseFromString(post.selftext_html || '', 'text/html').body
+            .textContent
+        "
+      ></p>
 
+      <div class="flex justify-between">
+        <p class="flex grow-0">
+          <StarIcon class="w-6 h-6 mr-2" />
+          {{ post.score }}
+        </p>
+        <p class="flex grow-0">
+          <ChatBubbleBottomCenterTextIcon class="w-6 h-6 mr-2" />
+          {{ post.num_comments }}
+        </p>
+        <p class="flex grow-0">
+          <PencilSquareIcon class="w-6 h-6 mr-2" />
+          {{ new Date(post.created * 1000).toLocaleDateString("sv-se") }}
+        </p>
+      </div>
+    </form>
+    <form method="dialog" class="modal-backdrop">
+      <button>close</button>
+    </form>
+  </dialog>
+</template>
